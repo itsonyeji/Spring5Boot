149497fe2f3736bc81124842f0962ba31a1677d8
itsonyeji <iqkeki@gmail.com>
2023-07-07 12:00:40 +0900

    회원가입 : MemberMapper 단위테스트 추가

---
+++ src/test/java/ityeji/hello/boot/spring5boot/MemberMapperUnitTest.java
@@ -0,0 +1,29 @@
+package ityeji.hello.boot.spring5boot;
+
+import ityeji.hello.boot.spring5boot.model.Member;
+import ityeji.hello.boot.spring5boot.mybatis.MemberMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)  /*자동으로 h2 데이터베이스를 사용하려고함. 내가 쓰는 데이터베이스로 조정해야함. replace속성으로*/
+public class MemberMapperUnitTest {
+
+    @Autowired
+    private MemberMapper memberMapper;
+
+    @Test
+    @DisplayName("MemberMapper insert Test")   /*명시될 test의 이름*/
+    void insertMember(){
+        Member m = new Member(null, "", "", "", "","","","","",null);
+        int result = memberMapper.insertMember(m);
+        System.out.println(result);
+        assertEquals(result, 1);
+    }
+
+}
