09f062ac55266993a6173c40159855fd44f08f09
itsonyeji <iqkeki@gmail.com>
2023-07-07 10:40:27 +0900

    회원가입 : MemberDAO 단위테스트 추가(실패)

---
+++ src/test/java/ityeji/hello/boot/spring5boot/MemberDAOUnitTest.java
@@ -0,0 +1,34 @@
+package ityeji.hello.boot.spring5boot;
+
+import ityeji.hello.boot.spring5boot.dao.MemberDAO;
+import ityeji.hello.boot.spring5boot.dao.MemberDAOImpl;
+import ityeji.hello.boot.spring5boot.model.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@MybatisTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)  /*자동으로 h2 데이터베이스를 사용하려고함. 내가 쓰는 데이터베이스로 조정해야함. replace속성으로*/
+@Import(MemberDAOImpl.class)
+public class MemberDAOUnitTest {
+
+    @Autowired private MemberDAO mdao;
+
+    @Test
+    @DisplayName("MemberDAO insert Test")   /*명시될 test의 이름*/
+    void insertMember(){
+        Member m = new Member(null, "", "", "", "","","","","",null);
+        int result = mdao.insertMember(m);
+        System.out.println(result);
+        assertEquals(result, 1);
+    }
+
+}
