cf834ab3d8dcce84fb909461afcb69ecb2562c02
itsonyeji <iqkeki@gmail.com>
2023-07-10 12:16:11 +0900

    우편번호 검색 엔터키 입력 차단

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -108,3 +108,9 @@ email3.addEventListener('click', () => {
         frm.email2.value=email3.value;
     }
 });
+
+// 우편번호 검색 엔터키 입력 차단
+dong?.addEventListener('keydown', (e)=>{
+    if(e.keyCode === 13)    // 엔터키(13)가 입력되면
+        e.preventDefault(); // 이벤트 전파 방지
+});
