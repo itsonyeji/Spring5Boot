4b50895c62ed019b8279f3f983da602513734b46
itsonyeji <iqkeki@gmail.com>
2023-07-10 11:35:44 +0900

    우편번호 조회 : 우편번호 검색 결과 보내기 - sendzip

--- src/main/resources/static/js/project.js
+++ src/main/resources/static/js/project.js
@@ -37,6 +37,7 @@ cancelbtn2?.addEventListener('click', ()=>{
 let zipbtn=document.querySelector('#findzipbtn');
 let dong=document.querySelector('#dong');
 let addrlist=document.querySelector('#addrlist');
+let sendzip=document.querySelector('#sendzip');
 const showzipaddr = (jsons) => {
     jsons=JSON.parse(jsons); //문자열을 json객체로 변환
     let addr='';
@@ -57,4 +58,19 @@ zipbtn?.addEventListener('click', ()=>{
     //url을 받아오고, 문자로 바꿈.
 
 
+});
+sendzip?.addEventListener('click', () => {
+    let frm=document.forms.joinfrm
+    let addr = addrlist.value;  //선택한 주소 항목
+    if (addr !== ''){
+        // 123-456 서울 관악구 신림동
+        let zip = addr.split(' ')[0];   // 우편번호 추출 : 공백으로 자른것의 첫번째를 가져온다.
+        let addrs=addr.split(' ');
+        let vaddr=`${addrs[1]} ${addrs[2]} ${addrs[3]}` // 주소 추출
+
+        frm.zip1.value=zip.split('-')[0];
+        frm.zip2.value=zip.split('-')[1];
+        frm.addr1.value=vaddr;
+    }else
+        alert('주소를 선택하세요!');
 });
