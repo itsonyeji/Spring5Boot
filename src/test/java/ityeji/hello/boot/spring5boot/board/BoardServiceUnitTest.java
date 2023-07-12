6d83651f00a5c128e5da1f1e1c1f44e8fc90f785
itsonyeji <iqkeki@gmail.com>
2023-07-12 10:16:38 +0900

    게시판 : BoardService 단위테스트

---
+++ src/test/java/ityeji/hello/boot/spring5boot/board/BoardServiceUnitTest.java
@@ -0,0 +1,39 @@
+package ityeji.hello.boot.spring5boot.board;
+
+import ityeji.hello.boot.spring5boot.dao.BoardDAOImpl;
+import ityeji.hello.boot.spring5boot.dao.MemberDAOImpl;
+import ityeji.hello.boot.spring5boot.model.Board;
+import ityeji.hello.boot.spring5boot.model.Member;
+import ityeji.hello.boot.spring5boot.service.BoardService;
+import ityeji.hello.boot.spring5boot.service.BoardServiceImpl;
+import ityeji.hello.boot.spring5boot.service.MemberService;
+import ityeji.hello.boot.spring5boot.service.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)  /*자동으로 h2 데이터베이스를 사용하려고함. 내가 쓰는 데이터베이스로 조정해야함. replace속성으로*/
+@Import({BoardServiceImpl.class, BoardDAOImpl.class})    /*이걸 써야 autowired에 주입이 됨*/
+public class BoardServiceUnitTest {
+
+    @Autowired private BoardService bsrv;
+
+    @Test
+    @DisplayName("BoardService read Test")
+    void readBoard(){
+        List<Board> results = bsrv.readBoard();
+
+        //System.out.println(results);
+        assertNotNull(results);
+    }
+}
