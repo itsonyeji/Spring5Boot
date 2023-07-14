ea2cab4a6687de330d3458a6476cb57bd59c9611
itsonyeji <iqkeki@gmail.com>
2023-07-14 16:08:31 +0900

    자료실 - PdsMapper insertPds 구현

---
+++ src/main/java/ityeji/hello/boot/spring5boot/mybatis/PdsMapper.java
@@ -0,0 +1,23 @@
+package ityeji.hello.boot.spring5boot.mybatis;
+
+import ityeji.hello.boot.spring5boot.model.Pds;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface PdsMapper {
+    int insertPds(Pds p);
+
+    int lastPdsPno();
+
+   /* List<Pds> selectPds(int stnum);
+    Pds selectOnePds(String bno);
+    int updateViewPds(String bno);
+
+    int selectCountPds();
+    List<Pds> selectFindPds(Map<String, Object> params);
+    int countFindPds(Map<String, Object> params);*/
+
+}
