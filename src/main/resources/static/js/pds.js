6aa49a1effe236bc2a3841662eb31425f33badf4
itsonyeji <iqkeki@gmail.com>
2023-07-14 18:07:50 +0900

    자료실 - write 자바스크립트 추가

---
+++ src/main/resources/static/js/pds.js
@@ -0,0 +1,58 @@
+// pds view
+let newpdsbtn=document.querySelector('#newpdsbtn');
+let modpdsbtn = document.querySelector('#modpdsbtn');
+let rmvpdsbtn = document.querySelector('#rmvpdsbtn');
+let lstpdsbtn = document.querySelector('#lstpdsbtn');
+newpdsbtn?.addEventListener('click', ()=>{
+   location.href='/pds/write';
+});
+
+modpdsbtn?.addEventListener('click', ()=>{
+    if(confirm("정말 수정하시겠어요?")){
+        alert('아직 미지원 기능입니다.');
+    }
+    /*location.href='/pds/modify';*/
+})
+rmvpdsbtn?.addEventListener('click', ()=>{
+    if(confirm("정말 삭제하시겠어요?")){
+        alert('아직 미지원 기능입니다.');
+    }
+    //location.href='/pds/remove';
+})
+lstpdsbtn?.addEventListener('click', ()=>{
+   location.href='/pds/list/1';
+});
+
+
+// pds write
+let wrtpdsbtn = document.querySelector("#wrtpdsbtn");
+let cancelbtn3 = document.querySelector("#cancelbtn3");
+wrtpdsbtn?.addEventListener('click', ()=>{
+    let frm = document.forms.pdsfrm;
+    if(frm.title.value ==='') alert("제목을 입력하세요!");
+    else if(frm.contents.value === '') alert("본문을 입력하세요!");
+    else if(grecaptcha.getResponse() ==='') alert("자동쓰기방지를 체크하세요!");
+    else{
+       frm.method='post';
+       frm.enctype='multipart/form-data'    /* 첨부기능을 위해서 인코딩타입을 바꿈. 클라이언트가 텍스트와 파일들을 다중으로 보낸다. */
+       frm.submit();
+       alert('새글 등록이 완료되었습니다.');
+    }
+});
+cancelbtn3?.addEventListener('click', ()=>{
+    location.href="/pds/list/1";
+});
+
+// pds find
+let findtype = document.querySelector('#findtype');
+let findkey = document.querySelector('#findkey');
+let findbtn=document.querySelector('#findbtn');
+findbtn?.addEventListener('click', ()=>{
+   if(findkey.value ==='') alert('검색어를 입력하세요!');
+   else{
+       location.href=`/pds/find/${findtype.value}/${findkey.value}/1`;
+   }
+});
+
+
+
