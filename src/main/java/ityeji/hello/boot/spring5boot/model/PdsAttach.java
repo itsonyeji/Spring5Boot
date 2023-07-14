b00ce90464e8e0da49567fa7b41eec9eba997f9c
itsonyeji <iqkeki@gmail.com>
2023-07-14 16:05:42 +0900

    자료실 - PdsAttach 클래스 추가

---
+++ src/main/java/ityeji/hello/boot/spring5boot/model/PdsAttach.java
@@ -0,0 +1,18 @@
+package ityeji.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdsAttach {
+    private String pano;
+    private String pno;
+    private String fname;
+    private String ftype;
+    private String fsize;
+    private String fdown;
+
+}
