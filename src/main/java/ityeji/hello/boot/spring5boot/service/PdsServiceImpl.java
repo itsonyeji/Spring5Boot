30d9659c77631a57b90f0210c83c3034e525d4ac
itsonyeji <iqkeki@gmail.com>
2023-07-14 18:04:55 +0900

    자료실 - PdsServiceImpl : processUpload, insertPdsAttach 구현

--- src/main/java/ityeji/hello/boot/spring5boot/service/PdsServiceImpl.java
+++ src/main/java/ityeji/hello/boot/spring5boot/service/PdsServiceImpl.java
@@ -2,6 +2,8 @@ package ityeji.hello.boot.spring5boot.service;
 
 import ityeji.hello.boot.spring5boot.dao.PdsDAO;
 import ityeji.hello.boot.spring5boot.model.Pds;
+import ityeji.hello.boot.spring5boot.model.PdsAttach;
+import ityeji.hello.boot.spring5boot.utils.PdsUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +15,7 @@ import java.util.Map;
 public class PdsServiceImpl implements PdsService{
 
     final PdsDAO pdao;
+    final PdsUtils pdsUtils;    /* @RequiredArgsConstructor를 쓰면 lombok이 두개를 받는 생성자를 만듦.? */
     @Override
     public int newPds(Pds p) {
 
@@ -23,6 +26,13 @@ public class PdsServiceImpl implements PdsService{
     @Override
     public boolean newPdsAttach(MultipartFile attach, int pno) {
 
-        return false;
+        // 첨부한 파일을 지정한 위치에 저장 후 파일정보 리턴
+        PdsAttach pa = pdsUtils.processUpload(attach);
+        pa.setPno(pno+"");  // 문자화 - pno+"" 이렇게나 String.valueOf(pno) 이렇게 가능
+
+        // 첨부파일 정보를 디비에 저장
+        int pacnt = pdao.insertPdsdAttach(pa);
+
+        return (pacnt>0) ? true : false;
     }
 }
