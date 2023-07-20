064e6bd554c0b843d01a55fea8d6255a0be85a2e
itsonyeji <iqkeki@gmail.com>
2023-07-20 11:18:12 +0900

    갤러리 : Gallery / GalAttach 클래스 추가

---
+++ src/main/java/ityeji/hello/boot/spring5boot/model/GalAttach.java
@@ -0,0 +1,16 @@
+package ityeji.hello.boot.spring5boot.model;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class GalAttach {
+    private String gano;
+    private String gno;
+    private String fname;
+    private String fsize;
+
+}
