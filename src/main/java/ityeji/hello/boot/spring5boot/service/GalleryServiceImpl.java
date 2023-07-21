6161fec8557072ea2c984a7f22510cf8cc7ca224
itsonyeji <iqkeki@gmail.com>
2023-07-21 13:44:38 +0900

    갤러리 : readOneGallery 구현

--- src/main/java/ityeji/hello/boot/spring5boot/service/GalleryServiceImpl.java
+++ src/main/java/ityeji/hello/boot/spring5boot/service/GalleryServiceImpl.java
@@ -46,4 +46,9 @@ public class GalleryServiceImpl implements GalleryService {
         ga.setGno(gno+"");
         return (gdao.insertGalAttach(ga) > 0) ? true : false;
     }
+
+    @Override
+    public Gallery readOneGallery(String gno) {
+        return gdao.selectOneGallery(gno);
+    }
 }
